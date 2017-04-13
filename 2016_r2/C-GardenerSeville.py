5c40485602f9d701ef965533dfbc81f54e3b31d0
UkuleleTrip <ukuleletrip@gmail.com>
2017-04-13 17:15:27 +0900

    not finished...

--- 2016_r2/C-GardenerSeville.py
+++ 2016_r2/C-GardenerSeville.py
@@ -2,6 +2,78 @@
 # -*- coding:utf-8 -*-
 import sys
 
+class Node(object):
+    def __init__(self, hedge_i):
+        self.hedge_i = i
+        self.left = None
+        self.top = None
+        self.right = None
+        self.bottom = None
+
+    def next(self, fm, hedges):
+        if self.left == fm:
+            return self.top if hedges[self.hedge_i] == '/' else self.bottom
+        if self.top == fm:
+            return self.left if hedges[self.hedge_i] == '/' else self.right
+        if self.right == fm:
+            return self.bottom if hedges[self.hedge_i] == '/' else self.top
+        if self.bottom == fm:
+            return self.right if hedges[self.hedge_i] == '/' else self.left
+
+        print self.hedge_i, fm, '\n', self.left, self.top, self.right, self.bottom
+        return None
+
+def create_garden(R, C):
+    nodes = [[Node(j*R+i) for i in range(C)] for j in range(R)]
+
+    for i in range(R):
+        for j in range(C):
+            node = nodes[i][j]
+            node.left = nodes[i][j-1] if j>0 else R*2+C+(C-i)
+            node.right = nodes[i][j+1] if j<C-1 else R+i+1
+            node.top = nodes[i-1][j] if i>0 else j+1
+            node.bottom = nodes[i+1][j] if i<R-1 else R+C+(R-j)
+            print '*', node.left, node.right, node.top, node.bottom
+
+    return nodes
+
+def is_connected(garden, hedges, c1, c2):
+    if c1 <= R:
+        node = garden[0][c1-1]
+    elif c1 <= R+C:
+        node = garden[c1-C-1][C-1]
+    elif c1 <= R*2+C:
+        node = garden[R-1][C-(c1-(R+C))]
+    else:
+        node = garden[R-(c1-(R*2+C))][0]
+    fm = c1
+
+    while True:
+        node = node.next(fm, hedges)
+        if type(node) == int:
+            return node == c2
+        fm = node
+
+
+def solve(R, C, courties):
+    garden = create_garden(R, C)
+    hedges = [' ']*(R*C)
+    for i in range(1<<(R*C)):
+        for j in range(R*C):
+            if (i>>j)&1:
+                hedges[j] = '/'
+            else:
+                hedges[j] = '\\'
+
+        for k in range(len(courties)/2):
+            if not is_connected(garden, hedges, courties[k*2], courties[k*2+1]):
+                break
+        else:
+            # all connected !!
+            return hedges
+
+    return None
+
 def print_garden(garden):
     for r in garden:
         print ''.join(r)
@@ -16,34 +88,6 @@ def set_hedge(garden, hedge, r, c):
         return False
     garden[r][c] = hedge
     return True
-
-def solve(R, C, courties):
-    garden = []
-    for i in range(R):
-        garden.append(['.']*C)
-
-    # decide corners
-    # top left
-    hedge = '/' if is_lover(courties, 1, R*2+C*2) else '\\'
-    if not set_hedge(garden, hedge, 0, 0):
-        return None
-
-    # top right
-    hedge = '\\' if is_lover(courties, C, C+1) else '/'
-    if not set_hedge(garden, hedge, 0, C-1):
-        return None
-
-    # bottom left
-    hedge = '\\' if is_lover(courties, C*2+R, C*2+R+1) else '/'
-    if not set_hedge(garden, hedge, R-1, 0):
-        return None
-
-    # bottom right
-    hedge = '/' if is_lover(courties, R+C+1, R+C) else '\\'
-    if not set_hedge(garden, hedge, R-1, C-1):
-        return None
-
-    return garden
 
 if __name__ == '__main__':
     f = open(sys.argv[1])
