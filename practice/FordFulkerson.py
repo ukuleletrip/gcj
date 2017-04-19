d8a24b36ca83dee9ee0b6c33d97858adde8d6447
UkuleleTrip <ukuleletrip@gmail.com>
2017-04-19 16:27:26 +0900

    share forward, reverse buffer version.

--- practice/FordFulkerson.py
+++ practice/FordFulkerson.py
@@ -2,6 +2,8 @@
 # -*- coding:utf-8 -*-
 import sys
 
+# http://even-eko.hatenablog.com/entry/2013/08/08/195120 was helpful
+
 Q1 = [[0, 3, 2, 0, 0, 0],
       [0, 0, 0, 2, 2, 0],
       [0, 0, 0, 2, 3, 0],
@@ -15,40 +17,26 @@ Q2 = [[0, 10, 0, 2, 0],
       [0, 0, 0, 0, 5],
       [0, 0, 0, 0, 0]]
 
-used = None
-rev = None
-
-def dfs(problem, frm, to, f):
+def dfs(problem, used, frm, to, f):
     if frm == to:
         # goal
         return f
     used[frm] = True
     for i in range(len(problem[frm])):
-        if not used[i] and (problem[frm][i] > 0 or rev[frm][i] > 0):
-            if problem[frm][i]:
-                d = dfs(problem, i, to, min(f, problem[frm][i]))
-                if d:
-                    problem[frm][i] -= d
-                    rev[i][frm] += d
-                    return d
-            else:
-                d = dfs(problem, i, to, min(f, rev[frm][i]))
-                if d:
-                    rev[frm][i] -= d
-                    problem[i][frm] += d
-                    return d
-
+        if not used[i] and problem[frm][i] > 0:
+            d = dfs(problem, used, i, to, min(f, problem[frm][i]))
+            if d:
+                problem[frm][i] -= d
+                problem[i][frm] += d
+                return d
     return 0
 
 def solve(problem, s, t):
-    global used, rev
-
-    rev = [[0 for j in range(len(problem[0]))] for i in range(len(problem))]
     flow = 0
     while True:
         used = [False]*len(problem)
-        f = dfs(problem, s, t, sys.maxint)
-        print problem, rev
+        f = dfs(problem, used, s, t, sys.maxint)
+        print problem
         if f == 0:
             break
         flow += f
